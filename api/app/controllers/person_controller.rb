e147ec32558e87894baf600a949fb7c799a07b2f
Bernice Anne W. Chua <email": null>
2016-02-17 12:16:01 -0800

    Add code to PersonController.

--- api/app/controllers/person_controller.rb
+++ api/app/controllers/person_controller.rb
@@ -1,2 +1,11 @@
 class PersonController < ApplicationController
+  def index
+    @persons = Person.all
+    render json: @persons
+  end
+
+  def show
+    @person = Person.find(params[:id])
+    render json: @person, include: :badges
+  end
 end
