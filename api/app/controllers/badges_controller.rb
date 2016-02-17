5963408dfd2955b53f11deeb4ac7285031f199c7
lindsey-s <ly.stevenson@gmail.com>
2016-02-16 19:21:04 -0800

    complete setup of controllers - update model and schema to delete Cohort model

--- api/app/controllers/badges_controller.rb
+++ api/app/controllers/badges_controller.rb
@@ -1,2 +1,48 @@
 class BadgesController < ApplicationController
+
+  before_action :set_badge, only: [:show, :update, :destroy]
+
+  def index
+    @badges = Badge.all
+    render json: @badges
+  end
+
+  def show
+    render json: @badge
+  end
+
+  def create
+    @badge = Badge.new(badge_params)
+    if @badge.save
+      render json: @badge, status: :created, location: @badge
+    else
+      errs
+    end
+  end
+
+  def update
+    if @badge.update(badge_params)
+      head :no_content
+    else
+      errs
+    end
+  end
+
+  def destroy
+    @badge.destroy
+    head :no_content
+  end
+
+  private
+  def set_badge
+    @badge = Badge.find(params[:id])
+  end
+
+  def badge_params
+    params.permit(:student_id, :text, :votes)
+  end
+
+  def errs
+    render json: @badge.errors, status: :unprocessable_entity
+  end
 end
--- api/app/controllers/students_controller.rb
+++ api/app/controllers/students_controller.rb
@@ -1,11 +1,48 @@
 class StudentsController < ApplicationController
 
+  before_action :set_student, only: [:show, :update, :destroy]
+
   def index
-    @students = Student.find_by(cohort_id: params[:id])
+    @students = Student.all
+    render json: @students
   end
 
   def show
-    @student = Student.find(params[:student_id])
+    render json: @student
   end
 
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      render json: @student, status: :created, location: @student
+    else
+      errs
+    end
+  end
+
+  def update
+    if @student.update(student_params)
+      head :no_content
+    else
+      errs
+    end
+  end
+
+  def destroy
+    @student.destroy
+    head :no_content
+  end
+
+  private
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.permit(:first_name, :last_name)
+  end
+
+  def errs
+    render json: @studnet.errors, status: :unprocessable_entity
+  end
 end
