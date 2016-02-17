8753c55a4154038c5ea7e76d505ca3da654a998f
Bernice Anne W. Chua <email": null>
2016-02-17 12:12:43 -0800

    Add associations to models of Badge & Person.

--- api/app/models/badge.rb
+++ api/app/models/badge.rb
@@ -1,12 +1,12 @@
 class Badge < ActiveRecord::Base
   belongs_to :person
 
-  def count_votes
-    if upvote
-      :vote += 1
-    else
-      :vote -= 1
-    end
-  end
+  # def count_votes
+  #   if upvote
+  #     :vote += 1
+  #   else
+  #     :vote -= 1
+  #   end
+  # end
 
 end
