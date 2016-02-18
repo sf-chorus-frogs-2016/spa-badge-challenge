a670ea31f849e35e36eb4b98a82881615a696267
iregina <reginawong14@gmail.com>
2016-02-18 07:33:48 -0800

    add student list to front end

--- api/config/application.rb
+++ api/config/application.rb
@@ -6,8 +6,17 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Api
   class Application < Rails::Application
+
+    config.api_only = true
+    config.middleware.insert_before 0, 'Rack::Cors' do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options, :patch, :head]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+10.times do
+	User.create(name: Faker::Name.name)
+end
+
+100.times do |i|
+	i = rand(10)
+	Badge.create(description: Faker::Company.bs, user_id: i )
+end
