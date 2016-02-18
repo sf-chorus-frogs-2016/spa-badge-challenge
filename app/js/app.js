05295f6c71909f4e55d4e5d71ce35cfe190dde40
lindsey-s <ly.stevenson@gmail.com>
2016-02-17 20:37:11 -0800

    implement home link functionality

--- app/js/app.js
+++ app/js/app.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(event){
 });
 
 var displayAllStudents = function() {
-  var request = miniQuery.AjaxWrapper.request({
+  miniQuery.AjaxWrapper.request({
     url: 'http://localhost:3000/students',
     type: 'GET',
   }).then(function(response) {
@@ -14,20 +14,16 @@ var displayAllStudents = function() {
     var context = {
       students: JSON.parse(response)
     }
-
     // console.log(context)
     // => parsed JSON - array of Objects
     var theTemplateScript = miniQuery.SweetSelector.html("#list-all-studs")
-
     var theTemplate = Handlebars.compile(theTemplateScript)
     // console.log(theTemplate)
-    // => the complie fxn from Handlebars
-
+      // => the complie fxn from Handlebars
     // now pass the context (parsed JSON) to the compile fxn
     var theCompiledHTML = theTemplate(context)
-
     // and replace the html on the DOM
-    miniQuery.SweetSelector.select("#student-list").innerHTML = theCompiledHTML
+    miniQuery.SweetSelector.select("#content-space").innerHTML = theCompiledHTML
 
   }).catch(function(error) {
     console.log("Error! " + error)
@@ -36,21 +32,43 @@ var displayAllStudents = function() {
 
 var bindListeners = function() {
   // console.log("Listeners BOUND")
-
   // attach event handler to click of student link
-  miniQuery.SweetSelector.select("#student-list").onclick = showStudent
-  miniQuery.SweetSelector.select("#vote-on").onsubmit = voteOnBadge
+  miniQuery.SweetSelector.select("#content-space").onclick = function(event) {
+    event.preventDefault();
+    event = event || window.event
+    var target = event.target || event.srcElement
+
+    if (target.nodeName != "A") {
+      return;
+    }
+    var elementClass = target.getAttribute("class")
+    console.log(elementClass)
+    switch (elementClass) {
+      case "single-student":
+        console.log("clicked on single-student class")
+        showStudent()
+        break;
+      case "home-btn":
+        console.log("clicked on Home button")
+        goHome()
+        break;
+      default:
+        alert("Please click a link or button")
+        break;
+    }
+  }
+
+  // miniQuery.SweetSelectorr.select(".container").onsubmit = submitNewBadge
 }
 
-var showStudent = function(event) {
-  event.preventDefault();
+var showStudent = function() {
   event = event || window.event
   var target = event.target || event.srcElement
   // console.log(target)
   // a tag on which user clicked
 
-  var url = target.getAttribute("href")
-  var student_id = url.match(/\d+/)[0]
+  var student_id = target.getAttribute("id")
+  // console.log(student_id)
 
   var request = miniQuery.AjaxWrapper.request({
     url: 'http://localhost:3000/students/' + student_id,
@@ -71,9 +89,16 @@ var showStudent = function(event) {
   })
 }
 
-var voteOnBadge = function(event) {
-  event.preventDefault();
+var goHome = function() {
+  console.log("Going Home!")
+  displayAllStudents()
+}
+
+var voteOnBadge = function() {
+  event = event || window.event
+  var target = event.target || event.srcElement
   console.log("I Voted!")
+  console.log(target)
 }
 
 
