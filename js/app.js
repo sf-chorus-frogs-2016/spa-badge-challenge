e5f6ecf3ff823925fd49b059f1d43d79b6ab9a19
Ovidio Calvo <OviWan@Ovidios-MacBook-Air.local>
2016-02-17 20:58:35 -0800

    Add javascript and html pages

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('student-page').hide();
+
+  var badgeText = $('#badge-template').html();
+  var badgeTemplate = Handlebars.compile(badgeText);
+  var nameText = $('#home-template').html();
+  var nameTemplate = Handlebars.compile(nameText);
+  var introText = $('#intro-template').html();
+  var introTemplate = Handlebars.compile(introText);
+  var addBadgeText = $('#badge')
+
+
+
+
+
+
+
+
+
+
+})
